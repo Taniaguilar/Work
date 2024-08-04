c031b27f7dd1ad2a2381519dffdb14c03a5ae9ed
Nia <taniallison@hotmail.com>
2024-08-04 02:30:09 -0500

    input y print

---
+++ input_print.py
@@ -0,0 +1,6 @@
+#solicitar y mostrar datos
+edad = int(input("Ingresa tu edad: \n"))
+if edad > 17 : print("Eres mayor de edad, tu edad es ",edad)
+else : print("Eres menor de edad, tu edad es ",edad)
+
+
