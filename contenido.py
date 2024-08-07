906027834940e9fd6e9cb860359536d66d2bba48
Nia <taniallison@hotmail.com>
2024-08-07 00:19:42 -0500

    IMC

---
+++ contenido.py
@@ -0,0 +1,9 @@
+#Calculadora de IMC
+
+print("Calculadora de IMC")
+peso = int(input("Ingresa tu peso\n"))
+estatura = int(input("Ingresa tu estatura\n"))
+imc = peso/estatura
+print("Tu imc es: ",imc)
+
+
--- input_print.py
+++ input_print.py
@@ -3,4 +3,3 @@ edad = int(input("Ingresa tu edad: \n"))
 if edad > 17 : print("Eres mayor de edad, tu edad es ",edad)
 else : print("Eres menor de edad, tu edad es ",edad)
 
-
